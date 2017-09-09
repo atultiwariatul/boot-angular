ab9963dbd819fb70927e97ffb6c04700e627647f
Atul Tiwari <mboapplication1@gmail.com>
2017-09-09 02:13:53 -0500

    First Commit

---
+++ src/main/java/com/xprhub/web/BootAngularApplication.java
@@ -0,0 +1,12 @@
+package com.xprhub.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootAngularApplication.class, args);
+	}
+}
---
+++ src/main/java/com/xprhub/web/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.xprhub.web;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BootAngularApplication.class);
+	}
+
+}
